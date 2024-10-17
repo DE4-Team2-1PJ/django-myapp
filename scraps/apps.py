ca8cfdc2f9a13aec44d5da9fc9de75179265cd1d
EKRHKD <ekrhkd2343@gmail.com>
2024-10-17 15:54:07 +0900

    modified:   recruits/apps.py 	modified:   recruits/templates/job_graph.html 	modified:   scraps/apps.py

--- scraps/apps.py
+++ scraps/apps.py
@@ -14,7 +14,7 @@ class ScrapsConfig(AppConfig):
 
     #when django start, it starts too.
     def ready(self):
-        from scraps.scrapers.specific_scraper import TheteamsScraper, SaraminScraper
+        from scraps.scrapers.specific_scraper import TheteamsScraper, SaraminScraper, IncruitScraper
         #스크래퍼 인스턴스 생성
         theTeamScraper = TheteamsScraper()
         # 60초마다 scrap 메서드 실행 #test10초
@@ -22,3 +22,6 @@ class ScrapsConfig(AppConfig):
         
         saraminScraper = SaraminScraper()
         saraminScraper.start_scraper_scheduler(interval_seconds=30)
+
+        incruitScraper = IncruitScraper()
+        incruitScraper.start_scraper_scheduler(interval_seconds=10)
